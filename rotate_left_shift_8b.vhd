4eb778668a0d9c039c06e873794b72d915dc2484
swl02 <github_email@example.com>
2020-03-29 12:11:29 +0800

    shifter done

--- rotate_left_shift_8b.vhd
+++ rotate_left_shift_8b.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.all;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -34,12 +36,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity rotate_left_shift_8b is
     Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
            sft : in STD_LOGIC_VECTOR (2 downto 0);
-           B : in STD_LOGIC_VECTOR (0 downto 0));
+           B : out STD_LOGIC_VECTOR (7 downto 0));
 end rotate_left_shift_8b;
 
 architecture Behavioral of rotate_left_shift_8b is
 
 begin
-
+		shift_proc:
+		process (A,sft)
+		begin
+		case sft is
+         when "001" => B <= std_logic_vector(unsigned(A) rol 1);
+         when "010" => B <= std_logic_vector(unsigned(A) rol 2);
+         when "011" => B <= std_logic_vector(unsigned(A) rol 3);
+         when "100" => B <= std_logic_vector(unsigned(A) rol 4);
+         when "101" => B <= std_logic_vector(unsigned(A) rol 5);
+         when "110" => B <= std_logic_vector(unsigned(A) rol 6);
+         when "111" => B <= std_logic_vector(unsigned(A) rol 7);
+			when others => B <= A;
+		end case;
+		end process;
 
 end Behavioral;
