663468873a3b604bd1b3389dad17ce5393e2bd8f
Elton Shih <z5187581@student.unsw.edu.au>
2020-03-29 14:20:20 +1100

    regfile code added

--- register_file.vhd
+++ register_file.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -44,8 +46,39 @@ entity register_file is
 end register_file;
 
 architecture Behavioral of register_file is
-
+    type reg_file is array(0 to 15) of std_logic_vector(15 downto 0);
+    signal sig_regfile : reg_file;
 begin
-
+    mem_process : process (reset, clk, read_register_a, read_register_b,
+                    write_enable, write_register, write_data) is
+    variable var_regfile        : reg_file;
+    variable var_read_addr_a    : integer;
+    variable var_read_addr_b    : integer;
+    variable var_write_addr     : integer;
+    
+    begin
+        var_read_addr_a := conv_integer(read_register_a);
+        var_read_addr_b := conv_integer(read_register_b);
+        var_write_addr  := conv_integer(write_register);
+        
+        if (reset = '1') then
+            -- initial values of the registers - reset to zeroes
+            var_regfile := (others => X"0000");
+        elsif (falling_edge(clk) and write_enable = '1') then
+            -- register write on the falling clock edge
+            var_regfile(var_write_addr) := write_data;
+        end if;
+        
+        -- enforces value zero for register $0
+        var_regfile(0) := X"0000";
+        
+        -- continuous read of the registers at location read_register_a
+        -- and read_register_b
+        read_data_a <= var_regfile(var_read_addr_a);
+        read_data_b <= var_regfile(var_read_addr_b);
+        
+        -- the following are probe signals (for simulation purpose)
+        sig_regfile <= var_regfile;
+    end process;
 
 end Behavioral;
