1169d38544530a63fe71d8f8f9825ee6187ff720
swl02 <github_email@example.com>
2020-04-08 06:50:15 +0800

    IF implementing

--- pipelined_core.vhd
+++ pipelined_core.vhd
@@ -38,7 +38,87 @@ end pipelined_core;
 
 architecture Behavioral of pipelined_core is
 
+component program_counter is
+    port ( reset    : in  std_logic;
+           clk      : in  std_logic;
+           addr_in  : in  std_logic_vector(3 downto 0);
+           addr_out : out std_logic_vector(3 downto 0) );
+end component;
+
+component adder_4b is
+    Port ( src_a : in STD_LOGIC_VECTOR (3 downto 0);
+           src_b : in STD_LOGIC_VECTOR (3 downto 0);
+           sum : out STD_LOGIC_VECTOR (3 downto 0);
+           carry_out : out STD_LOGIC );
+end component;
+
+
+component instruction_memory is
+    Port ( reset : in STD_LOGIC;
+           clk : in STD_LOGIC;
+           addr_in : in STD_LOGIC_VECTOR (3 downto 0);
+           insn_out : out STD_LOGIC_VECTOR (31 downto 0));
+end component;
+
+component pipe_if_id is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           instruction_in : in  STD_LOGIC_VECTOR (15 downto 0);
+           pc_in : in  STD_LOGIC_VECTOR (3 downto 0);
+           instruction_out : out  STD_LOGIC_VECTOR (15 downto 0);
+           pc_out : out  STD_LOGIC_VECTOR (3 downto 0));
+end component;
+
+
+
+
+signal sig_next_pc              : std_logic_vector(3 downto 0);
+signal sig_curr_pc              : std_logic_vector(3 downto 0);
+signal sig_one_4b               : std_logic_vector(3 downto 0);
+signal sig_if_pc					  : std_logic_vector(3 downto 0);
+signal sig_pc_carry_out         : std_logic;
+signal sig_insn                 : std_logic_vector(15 downto 0);
+signal sig_sign_extended_offset : std_logic_vector(15 downto 0);
+signal sig_if_insn				  : std_logic_vector(15 downto 0);
+
 begin
+
+	-------INSTRUCTION FETCHING-----------
+    sig_one_4b <= "0001";
+
+    pc : program_counter
+    port map ( reset    => reset,
+               clk      => clk,
+               addr_in  => sig_next_pc,
+               addr_out => sig_curr_pc ); 
+
+    next_pc : adder_4b 
+    port map ( src_a     => sig_curr_pc, 
+               src_b     => sig_one_4b,
+               sum       => sig_next_pc,   
+               carry_out => sig_pc_carry_out );
+					
+	 insn_mem : instruction_memory 
+    port map ( reset    => reset,
+               clk      => clk,
+               addr_in  => sig_curr_pc,
+               insn_out => sig_insn );
+					
+	 --pipelining
+	 pipeline_IF_ID :  pipe_if_id
+	 port map ( reset 	=> reset,
+					clk 		=> clk,
+					pc_in    => sig_next_pc,
+					instruction_in => sig_insn,
+					pc_out   => sig_if_pc,
+					instruction_out => sig_if_insn);
+	 
+	 
+				
+					
+	 
+	 -------INSTRUCTION DECODER-----------
+
 
 
 end Behavioral;
