967d03815c3802ae581249e7cef1367bc64e2817
swl02 <github_email@example.com>
2020-04-09 14:26:19 +0800

    pc

--- program_counter.vhd
+++ program_counter.vhd
@@ -11,6 +11,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity program_counter is
     Port ( reset    : in STD_LOGIC;
            clk      : in STD_LOGIC;
+			  pcwrite  : in STD_LOGIC;
            addr_in  : in STD_LOGIC_VECTOR (3 downto 0);
            addr_out : out STD_LOGIC_VECTOR (3 downto 0));
 end program_counter;
@@ -25,7 +26,7 @@ begin
     begin
        if (reset = '1') then
            addr_out <= (others => '0'); 
-       elsif (rising_edge(clk)) then
+       elsif (rising_edge(clk)) and pcwrite = '0' then
            addr_out <= addr_in; 
        end if;
     end process;
